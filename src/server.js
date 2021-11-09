0f99bb35834cb86a0cf548b91f83de3367a29fa8
nagendrax02 <rawatnagendra98@gmail.com>
2021-11-09 11:34:52 +0530

    changes to server.js and added db.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const dbConnection = require("./configs/db");
 require("dotenv").config();
+
 const app = express();
 
 const PORT = process.env.PORT || 2000;
 
-app.listen(PORT,()=>{
+app.listen(PORT,async()=>{
+    await dbConnection();
     console.log(`Listening to ${PORT}`);
 })
+
