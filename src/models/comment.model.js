46e5c2e0b7a5f19f073a217ddf88589948f33ff9
rinkubangale <bangalehemant@gmail.com>
2021-11-09 22:31:57 +0530

    newSchemas

---
+++ src/models/comment.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      minlength: [3, "Must be three characters long"],
+      required: [true, "Text is required"],
+    },
+    videoId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true }, timestamps: true }
+);
+
+CommentSchema.virtual("replies", {
+  ref: "Reply",
+  localField: "_id",
+  foreignField: "commentId",
+  justOne: false,
+
+  options: { sort: { createdAt: -1 } },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
