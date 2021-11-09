cadc236a3b8b2eaea827a47c310f7b4b7326255c
0x7EVEN <its.anonymus149@gmail.com>
2021-11-09 14:02:51 +0530

    adding userController

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 //
+
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+
+
+const connect = require("./configs/db");
+
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+app.listen(8080, async () => {
+     await connect();
+     console.log("listening on port 8080");
+});
