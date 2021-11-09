834917f5549a936caa77ceea159cf70638571e93
ar1fshaikh <76391543+0x7EVEN@users.noreply.github.com>
2021-11-09 14:40:00 +0530

    Delete configs directory

--- configs/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const connect = async () => {
-     // mongodb + srv://mpluser:01CkQDS5AghKuPME@cluster0.aywy9.mongodb.net/products?retryWrites=true&
-     // mongodb + srv://mpluser:3vjJRPTvxuwh2XTq@cluster0.aywy9.mongodb.net/products?retryWrites=true&w=majority",
-     // mongodb+srv://mpluser:01CkQDS5AghKuPME@cluster0.aywy9.mongodb.net/products?retryWrites=true&w=majority
-     return mongoose.connect("mongodb+srv://youtube:dbUserPassword@cluster0.qhdmg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-};
-module.exports = connect;
