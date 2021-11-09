528cafa30b28a81af7b56db3e7bac7c231557bf2
0x7EVEN <its.anonymus149@gmail.com>
2021-11-09 14:40:25 +0530

    removing redundent folders

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
+const router = express.Router();
 
 
+const User = require("/");
 
 
+router.get("/", async function(req, res) {
+
+});
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dbConnection = require("./configs/db");
-const categoryRoutes = require('./routes/category.route')
+const categoryRoutes = require('./routes/category.route');
 
 require("dotenv").config();
 
@@ -15,8 +15,8 @@ app.use('/api/v1/categories', categoryRoutes);
 
 const PORT = process.env.PORT || 2000;
 
-app.listen(PORT,async()=>{
+app.listen(PORT, async () => {
     await dbConnection();
     console.log(`Listening to ${PORT}`);
-})
+});
 
