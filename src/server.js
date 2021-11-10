0f812a5f397c2194103295261b41e9d935293c50
0x7EVEN <its.anonymus149@gmail.com>
2021-11-10 12:43:51 +0530

    resolving conflicts

--- src/server.js
+++ src/server.js
@@ -1,37 +1,29 @@
 const express = require("express");
 const dbConnection = require("./configs/db");
-<<<<<<< HEAD
 const categoryRoutes = require('./routes/category.route');
-=======
-const categoryRoutes = require('./routes/category.route')
-const errorHandler = require("./middlewares/error.middleware")
+const errorHandler = require("./middlewares/error.middleware");
 
 
 require("dotenv").config();
 
 
->>>>>>> 8daaa628925cd25ff1bab18b5d1be421c664d225
 const app = express();
 app.use(express.json());
 
-<<<<<<< HEAD
 const userController = require("./controllers/user.controller");
 
 app.use('/categories', categoryRoutes);
 
 app.use("/api/v1/users", userController);
-=======
-app.use('/categories', categoryRoutes);
-app.use(errorHandler)
+app.use(errorHandler);
 // app.use("/api/v1/users", userRoutes);
->>>>>>> 8daaa628925cd25ff1bab18b5d1be421c664d225
 
 
 const PORT = process.env.PORT || 2000;
 
-app.listen(PORT,async()=>{
+app.listen(PORT, async () => {
     await dbConnection();
     console.log(`Listening to ${PORT}`);
-})
+});
 
 
