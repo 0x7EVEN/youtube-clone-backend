3064c54aa481787700b865911962344308709f2a
rinkubangale <bangalehemant@gmail.com>
2021-11-10 13:31:26 +0530

    video controller

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const dbConnection = require("./configs/db");
-const categoryRoutes = require('./routes/category.route');
+const categoryRoutes = require("./routes/category.route");
 
 require("dotenv").config();
 
+const videoController = require("./controllers/video.controller.js");
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/videos", videoController);
 
-app.use('/categories', categoryRoutes);
+app.use("/categories", categoryRoutes);
 
-app.use("/api/v1/users", userRoutes);
-
+// app.use("/api/v1/users", userRoutes);
 
 const PORT = process.env.PORT || 2000;
-console.log('PORT:', PORT);
+console.log("PORT:", PORT);
 
 app.listen(PORT, async () => {
-    await dbConnection();
-    console.log(`Listening to ${PORT}`);
+  await dbConnection();
+  console.log(`Listening to ${PORT}`);
 });
-
