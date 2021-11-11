6ff23db62aa00fcccd76f1f8227d14468cfbd62f
0x7EVEN <its.anonymus149@gmail.com>
2021-11-11 15:50:15 +0530

    adding subscribe route

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // const {register, login} = require("./controllers/auth.controller");
 const videoController = require("./controllers/video.controller");
 const feelingController = require("./routes/feelings.route");
-
+const subscibersRoute = require("./routes/subscribe.routes");
 
 app.use("/videos", videoController);
 app.use("/feeling", feelingController);
@@ -27,11 +27,14 @@ app.use('/categories', categoryRoutes);
 app.use("/users", userController);
 // app.use("/register", register);
 // app.use("/login", login);
-
-
-// app.use("/api/v1/users", userRoutes);
+app.use("/subscribe", subscibersRoute);
 
 // app.use("/api/v1/users", userRoutes);
+
+// app.use("/api/v1/users", userRoutes);
+app.use(async function(req, res) {
+    res.send("404");
+});
 
 const PORT = process.env.PORT || 2000;
 console.log("PORT:", PORT);
